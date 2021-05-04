c5eb2677a314972bc74307cc44a52c01baaed7af
Mark <mark.litfin@yahoo.com>
2021-05-04 16:41:09 -0400

    updated cors

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.use(function (req, res, next) {
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
+    // Pass to next layer of middleware
+    next();
 });
 
 app.use(cors())
