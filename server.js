7dd759a22676dc9f75bff96f96d64bacdc96a105
Mark <mark.litfin@yahoo.com>
2021-05-04 16:08:05 -0400

    updated cors

--- server.js
+++ server.js
@@ -42,7 +42,13 @@ const userData = {
     name: "LocalUser"
 };
 
-app.use(cors({origin: 'null'}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+app.use(cors({origin: 'https://litgamers.org'}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
