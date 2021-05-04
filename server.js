5c37a0ab0ebf71c85f04f4cca813221effcb9a66
Mark <mark.litfin@yahoo.com>
2021-05-04 16:17:52 -0400

    updated cors

--- server.js
+++ server.js
@@ -75,10 +75,10 @@ app.get('/', (req, res) => {
 
 // validate the user credentials
 app.post('/users/signin', async function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); 
+    req.setHeader('Access-Control-Allow-Origin', '*');
+    req.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    req.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    req.setHeader('Access-Control-Allow-Credentials', true); 
     const user = req.body.username;
     // generate token
     const token = utils.generateToken(user);
