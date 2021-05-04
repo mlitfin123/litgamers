bb61512e16135004f22ae6c26c57eb69bdee8f4f
Mark <mark.litfin@yahoo.com>
2021-05-04 17:12:07 -0400

    updated cors

--- server.js
+++ server.js
@@ -17,12 +17,7 @@ const app = express();
 const userData = {
     name: "LocalUser"
 };
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
+
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
